81ad9c66d518225c846ce0357889dd53d251a89a
Ar4celi <araceliverenicea@gmail.com>
2023-10-16 03:40:46 -0300

    adelantando

--- inc/header.php
+++ inc/header.php
@@ -2,23 +2,23 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../css/estilo.css">
     <title>header</title>
 </head>
 <body>
     <header>
-     <a href="" class="logo">
-        <img src="../img/logo.png" alt="logo de la inmo">
-    </a>
-      <nav>
-        <a href="" class="nav-link">Inicio</a>
-        <a href="" class="nav-link">Comprar</a>
-        <a href="" class="nav-link">Alquilar</a>
-        <a href="" class="nav-link">Nosotros</a>
-        <a href="" class="nav-link">Contacto</a>
-      </nav>
+        <a href="index.php" class="logo">
+            <img src="img/logo.png" alt="logo de la inmo">
+        </a>
+        <nav>
+            <a href="index.php" class="nav-link">Inicio</a>
+            <a href="" class="nav-link">Comprar</a>
+            <a href="" class="nav-link">Alquilar</a>
+            <a href="" class="nav-link">Nosotros</a>
+            <a href="" class="nav-link">Contacto</a>
+        </nav>
     </header>
 </body>
 </html>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href=" css/estilo.css">
+    <link rel="stylesheet" type="text/css" href=" css/estilo2.css">
+
+    <title>Inmobiliaria</title>
+</head>
+<body>
+<?php
+include "inc/header.php";
+?>
+<div class="cont">
+    <div class="cont-img">
+<img src="img/fondoini.jpg" class="imgini" alt="fondo de inicio" >
+<a href="https://www.instagram.com/?hl=es-la">
+<img src="img/inst.png" class="imgsec1" alt="appig" >
+</a>
+<a href="https://www.facebook.com/">
+<img src="img/face.png" class="imgsec2" alt="appfc" >
+</a>
+<a href="https://web.whatsapp.com/">
+<img src="img/what.png" class="imgsec3"  alt="appwhatss" >
+</a>
+    </div>
+    <div class="frase">
+        <div class="texto">Bienvenido a INMOVISIÓN<br>
+Descubre el lugar<br> perfecto para llamar<br> hogar.</div>
+    </div>
+    <div class="frase2">
+        <div class="texto">Nuestra misión es encontrar el espacio perfecto que se ajuste a tu estilo de vida y necesidades.</div>
+    </div>
+</div>
+<div class="divisor">
+    <p>Casa es donde comienza tu historia. Encuéntrala con nosotros.</p>
+</div>
+
+</body>
+</html>
