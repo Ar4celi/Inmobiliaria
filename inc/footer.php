d1c311527fd81d4326a153ef448b7ed07f421b30
Ar4celi <araceliverenicea@gmail.com>
2023-10-16 21:02:49 -0300

    cheek

---
+++ inc/footer.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/estilof.css">
+    <title>Footer</title>
+    <script src="https://kit.fontawesome.com/b74ce0269d.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<footer>
+   <div class="cont1">
+ <div class="box">
+    <figure>
+        <a href="#">
+            <img src="img/logo.png" alt="logo de footer">
+        </a>
+    </figure>
+ </div>
+ <div class="box">
+    <h2>
+        SOBRE NOSOTROS    </h2>
+        <p>Inmobiliarias en Argentina
+publicados por profesionales del sector inmobiliario y usuarios particulares.
+Todo tipo de inmuebles Casas, departamentos, cocheras,
+oficinas, lotes, terenos, galpones, pisos, chalets.
+casas en venta - casas en alquiler - departamentos en alquiler - departamentos en venta
+inmobiliarias en la plata - inmuebles - propiedades en La Plata</p>
+    
+ </div>
+ <div class="box">
+    <h2>SIGUENOS</h2>
+    <div class="redsocial">
+        <a href="#" class="fa fa-facebook"></a>
+        <a href="#" class=" fa fa-instagram"></a>
+    
+            </div>
+        </div>
+   </div>
+   <div class="cont2"> 
+    <small>
+        &copy;2023 <b>INMOVISION</b> -Todos los derechos reservados-
+    </small>
+   </div>
+</footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href=" css/estilo.css">
     <link rel="stylesheet" type="text/css" href=" css/estilo2.css">
-
+    
     <title>Inmobiliaria</title>
 </head>
 <body>
@@ -15,7 +14,7 @@ include "inc/header.php";
 ?>
 <div class="cont">
     <div class="cont-img">
-<img src="img/fondoini.jpg" class="imgini" alt="fondo de inicio" >
+<img src="img/fondoini.png" class="imgini" alt="fondo de inicio" >
 <a href="https://www.instagram.com/?hl=es-la">
 <img src="img/inst.png" class="imgsec1" alt="appig" >
 </a>
@@ -38,10 +37,13 @@ Descubre el lugar<br> perfecto para llamar<br> hogar.
     <p>Casa es donde comienza tu historia. Encuéntrala con nosotros.</p>
 </div>
 <h2>Propiedades destacadas</h2>
-aqui van cositas  
+aqui van cositas 
 </div>
     <div class="divisor2">
-    <p>Te ofrecemos una experiencia de compra única, donde cada propiedad cuenta una historia y está esperando ser tu próximo hogar.Nuestra misión es simple pero apasionada: ayudarte a encontrar la propiedad perfecta, ese lugar especial donde puedas construir tus memorias, criar a tu familia y lograr tus objetivos de inversión. Con un equipo de profesionales altamente capacitados y comprometidos, estamos aquí para hacer que tu búsqueda de un hogar sea más fácil y emocionante..</p>
+    Te ofrecemos una experiencia de compra única, donde cada propiedad cuenta una historia y está esperando ser tu próximo hogar.Nuestra misión es simple pero apasionada: ayudarte a encontrar la propiedad perfecta, ese lugar especial donde puedas construir tus memorias, criar a tu familia y lograr tus objetivos de inversión. Con un equipo de profesionales altamente capacitados y comprometidos, estamos aquí para hacer que tu búsqueda de un hogar sea más fácil y emocionante.
 </div>
+<?php
+include "inc/footer.php";
+?>
 </body>
 </html>
